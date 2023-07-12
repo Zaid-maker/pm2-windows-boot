988b729d63b9c302e36a1fa6869a997475d8a3e2
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-07-12 23:19:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+const startOnBoot = require('start-on-windows-boot');
+const yargs = require('yargs');
+
+const argv = yargs
+  .usage('Usage: pm2-startup <command>')
+  .command('install', 'Adds a registry entry which resurrects PM2 on startup.')
+  .command('uninstall', 'Removes the registry entry which resurrects PM2 on startup.')
+  .demandCommand(1)
+  .argv;
