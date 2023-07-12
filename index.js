80339f86ff927da77b2e53d66781ecaf1ea3dc1a
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-07-12 23:23:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ const argv = yargs
 
 var applicationName = 'PM2';
 var applicationCommand = `wscript.exe "${__dirname}\\invisible.vbs" "${__dirname}\\pm2_resurrect.cmd"`;
+
+switch (argv[0]) {
+  case 'install':
+    enablePm2Startup();
+    break;
+
+  case 'uninstall':
+    removePm2Startup();
+    break;
+
+  default:
+    console.log('Invalid command. Please provide either "install" or "uninstall" as a command.');
+}
+
